255a71ca978db2a4686c11dfbdcd75cc598e47b7
pablodixs <pabllosoarez@gmail.com>
2025-06-24 19:38:43 -0300

    feat: create index and auth pages

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express'
+import mustacheExpress from 'mustache-express'
+import path from 'path'
+
+import highlightedGames from './utils/mock.json'
+
+const app = express()
+const PORT = 3333
+
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, '../views'))
+app.set('view cache', false)
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(highlightedGames)
+    res.render('index')
+})
+
+app.get('/auth', (req: Request, res: Response) => {
+    console.log(highlightedGames)
+    res.render('auth')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
